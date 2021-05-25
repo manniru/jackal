d356cf1870b4724c30f2a20e7d25b20c19222773
Tej <tkahlon@labx.com>
2021-05-24 22:19:32 -0400

    bump version

--- content/Links.js
+++ content/Links.js
@@ -51,7 +51,7 @@ const Links = () => {
           aria-label="Check latest version metrics"
           title="Check latest version metrics"
         >
-          1.9
+          1.10
         </a>
       </small>
     </>
