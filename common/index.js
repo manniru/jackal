6df73eea4d5a9e668e267e6c678ebb3d1d759837
Tej <tkahlon@labx.com>
2021-07-07 18:13:17 -0400

    v1.14 - cleanup

--- common/index.js
+++ common/index.js
@@ -9,89 +9,8 @@ const getTextFromFetch = async (url) => {
   return text;
 };
 
-const methodForEXTVLCOPT = (i, brokenIndex) => {
-  const textString = i
-    .split("#")
-    .filter((j) => j)
-    .filter((j) => (j.includes("EXTM3U") ? null : j))
-    .filter((i, idx) => idx === brokenIndex)
-    .map((j) => {
-      const text = j.split('",')[1];
-      const finalText = text.replace(/\n/g, "");
-      return finalText;
-    });
-  const urlString = i
-    .split("#")
-    .filter((j) => j)
-    .filter((j) => (j.includes("EXTM3U") ? null : j))
-    .filter((i, idx) => idx === brokenIndex + 1)
-    .map((j) => {
-      const array = j.split(",");
-      if (array.length === 1) {
-        return array[0];
-      } else {
-        const url = array[1];
-        return url;
-      }
-    });
-  const newUrlString = urlString[0];
-  const getUrl = newUrlString.split("http")[1];
-  const cleanUrl = getUrl.replace(/\n/g, "");
-  const finalUrl = textString[0] + "\n" + "http" + cleanUrl;
-  return finalUrl;
-};
-
-const parseXLinks = (myPromise, codes) => {
-  return myPromise.map((i, index) => {
-    return i
-      .split("#")
-      .filter((j) => (j !== "" && j.includes("EXTM3U") ? null : j))
-      .map((j, index) => {
-        const array = j.split(",");
-        const type = array[0];
-        const url = array[1];
-        if (url && !url.includes("://")) {
-          return {
-            type,
-            url: methodForEXTVLCOPT(i, index),
-          };
-        }
-        return { type, url };
-      })
-      .filter((j) => (typeof j.url === undefined ? null : j))
-      .map((j) => {
-        let word;
-        let myUrl;
-        let myTitle;
-        if (j.type && j.type.includes("group-title=")) {
-          const array = j.type.split("group-title=");
-          word = array[1].replace(/"/g, "");
-          word = word.trim() ? word : "None";
-        } else {
-          word = "None";
-        }
-        if (j.url && j.url.includes("\n")) {
-          myTitle = j.url.split("\n")[0].trim();
-          myUrl = j.url.split("\n")[1].trim();
-        } else {
-          myUrl = j.url;
-          myTitle = "Unknown";
-        }
-        return {
-          type: word,
-          url: myUrl,
-          title: myTitle,
-          country: codes[index],
-        };
-      })
-      .filter((j) => j.url && j.type !== "XXX")
-      .sort((a, b) => (a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1))
-      .filter((j) => (j.title.includes("XXX") ? false : j));
-  });
-};
-
-const parseLinks = async (myPromise) => {
-  let codes = myPromise
+const getCountryCodes = (array) => {
+  return array
     .split("#")
     .map((i) => i.replace(/\n/gi, ""))
     .filter((i) => i !== "")
@@ -100,7 +19,10 @@ const parseLinks = async (myPromise) => {
     .map((i) => i[0])
     .map((i) => i.split(","))
     .map((i) => i[1]);
-  const urls = myPromise
+};
+
+const getChannelUrls = (array) => {
+  return array
     .split("#")
     .map((i) => i.replace(/\n/gi, ""))
     .map((i) => i.replace(/EXTINF:-1,/gi, ""))
@@ -114,29 +36,107 @@ const parseLinks = async (myPromise) => {
       (i) =>
         `https://raw.githubusercontent.com/iptv-org/iptv/master/channels/${i}.m3u`
     );
+};
+
+const getChannelUrlForMethod = (i, brokenIndex) => {
+  return i
+    .split("#")
+    .filter((j) => j)
+    .filter((j) => (j.includes("EXTM3U") ? null : j))
+    .filter((j, idx) => idx === brokenIndex + 1)
+    .map((j) => {
+      const array = j.split(",");
+      if (array.length === 1) {
+        return array[0];
+      } else {
+        const url = array[1];
+        return url;
+      }
+    });
+};
+
+const getChannelNameForMethod = (i, brokenIndex) => {
+  return i
+    .split("#")
+    .filter((j) => j)
+    .filter((j) => (j.includes("EXTM3U") ? null : j))
+    .filter((j, idx) => idx === brokenIndex)
+    .map((j) => {
+      const text = j.split('",')[1];
+      const finalText = text.replace(/\n/g, "");
+      return finalText;
+    });
+};
+
+const methodForEXTVLCOPT = (i, brokenIndex) => {
+  const channelName = getChannelNameForMethod(i, brokenIndex);
+  const channelUrl = getChannelUrlForMethod(i, brokenIndex);
+  const newUrlString = channelUrl[0];
+  const getUrl = newUrlString.split("http")[1];
+  const cleanUrl = getUrl.replace(/\n/g, "");
+  const finalUrl = channelName[0] + "\n" + "http" + cleanUrl;
+  return finalUrl;
+};
+
+const getSpecialMethodChannels = (i, j, index) => {
+  const array = j.split(",");
+  const type = array[0];
+  const url = array[1];
+  if (url && !url.includes("://")) {
+    return {
+      type,
+      url: methodForEXTVLCOPT(i, index),
+    };
+  }
+  return { type, url };
+};
+
+const cleanUpListing = (j, index, codes) => {
+  let word;
+  let myUrl;
+  let myTitle;
+  if (j.type && j.type.includes("group-title=")) {
+    const array = j.type.split("group-title=");
+    word = array[1].replace(/"/g, "");
+    word = word.trim() ? word : "None";
+  } else {
+    word = "None";
+  }
+  if (j.url && j.url.includes("\n")) {
+    myTitle = j.url.split("\n")[0].trim();
+    myUrl = j.url.split("\n")[1].trim();
+  } else {
+    myUrl = j.url;
+    myTitle = "Unknown";
+  }
+  return {
+    type: word,
+    url: myUrl,
+    title: myTitle,
+    country: codes[index],
+  };
+};
+
+const parseXLinks = (myPromise, codes) => {
+  return myPromise.map((i, index) => {
+    return i
+      .split("#")
+      .filter((j) => (j !== "" && j.includes("EXTM3U") ? null : j))
+      .map((j, index) => getSpecialMethodChannels(i, j, index))
+      .filter((j) => (typeof j.url === undefined ? null : j))
+      .map((j) => cleanUpListing(j, index, codes))
+      .filter((j) => j.url && j.type !== "XXX")
+      .sort((a, b) => (a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1))
+      .filter((j) => (j.title.includes("XXX") ? false : j));
+  });
+};
+
+const parseLinks = async (myPromise) => {
+  const codes = getCountryCodes(myPromise);
+  const urls = getChannelUrls(myPromise);
   const promises = urls.map(getTextFromFetch);
   const results = await Promise.all(promises);
   return parseXLinks(results, codes);
-};
-
-// https://stackoverflow.com/questions/51805395/navigator-clipboard-is-undefined
-const copyToClipboard = (textToCopy) => {
-  if (navigator.clipboard && window.isSecureContext) {
-    return navigator.clipboard.writeText(textToCopy);
-  } else {
-    let textArea = document.createElement("textarea");
-    textArea.value = textToCopy;
-    textArea.style.position = "fixed";
-    textArea.style.left = "-999999px";
-    textArea.style.top = "-999999px";
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-    return new Promise((res, rej) => {
-      document.execCommand("copy") ? res() : rej();
-      textArea.remove();
-    });
-  }
 };
 
 const getData = async () => {
@@ -156,6 +156,26 @@ const getData = async () => {
     return finalList;
   } catch (e) {
     return "fail";
+  }
+};
+
+// https://stackoverflow.com/questions/51805395/navigator-clipboard-is-undefined
+const copyToClipboard = (textToCopy) => {
+  if (navigator.clipboard && window.isSecureContext) {
+    return navigator.clipboard.writeText(textToCopy);
+  } else {
+    let textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    textArea.style.position = "fixed";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    return new Promise((res, rej) => {
+      document.execCommand("copy") ? res() : rej();
+      textArea.remove();
+    });
   }
 };
 
